b860ded927bf7103076acbad95efcd8e890299eb
kelwin Esteves de Souza <117915056+Niwkel@users.noreply.github.com>
2024-12-05 00:22:25 -0300

    Add files via upload

---
+++ pedido do best/script.js
@@ -0,0 +1,7 @@
+document.getElementById('yes').addEventListener('click', function() {
+    window.location.href = 'best-friends.html';
+});
+
+document.getElementById('no').addEventListener('click', function() {
+    // Botão "Não" parece funcional, mas não faz nada
+});
